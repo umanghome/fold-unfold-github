220cb29a47ea1ce4d3d4cfec1d4961b61aa230da
Umang Galaiya <umang.galaiya@razorpay.com>
2018-05-09 01:59:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# GitHub Fold
+
+Fold or Unfold all files in GitHub's compare view.
+
+---
+
+I like reviewing each file individually while in GitHub's compare view and having all files open and expanded all at once is distracting. _(also sometimes my browser gives up when the diffs are huge.)_
+
+This extension adds buttons to fold or unfold all files at once.
---
+++ popup.js
@@ -0,0 +1,35 @@
+/**
+ * Generate the code.
+ * @param {String} bool
+ * @return {String}
+ */
+function generateCode (bool) {
+  return "document.querySelectorAll('.btn-octicon.p-1.pr-2.js-details-target[aria-expanded=" + bool + "]').forEach(e => e.click());";
+}
+
+/**
+ * Generate the listener.
+ * @param {String} code 
+ * @return {Function}
+ */
+function generateListener (code) {
+  return function () {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        {
+          code: code
+        }
+      );
+    });
+  }
+}
+
+const fold = generateCode('true');
+const unfold = generateCode('false');
+
+document.querySelector('#fold').addEventListener('click', generateListener(fold));
+document.querySelector('#unfold').addEventListener('click', generateListener(unfold));
